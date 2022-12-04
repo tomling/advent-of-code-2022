7e586f19aeb9440d40aff5f35481465427081e7e
Tom <19222781+tomling@users.noreply.github.com>
2022-12-04 18:02:59 +0000

    Day 2 Progress

--- src/Day2/RockPaperScissors.cs
+++ src/Day2/RockPaperScissors.cs
@@ -2,9 +2,49 @@
 {
     public static class RockPaperScissors
     {
-        public static int CalculateTask1(string input)
+        public static int CalculatePart1(string input)
         {
-            return 0;
+            var splitInput = input.Split(" ");
+
+            return CalculateScore(splitInput[0], splitInput[1]);
         }
+        //Opponent Choices
+        //A for Rock, B for Paper, and C for Scissors.
+        //Player Choices
+        //X for Rock, Y for Paper, and Z for Scissors
+        private static int CalculateScore(string opponentChoice, string playerChoice)
+        {
+            var score = 0;
+
+            switch (playerChoice)
+            {
+                case "X":
+                    score += 1;
+                    break;
+                case "Y":
+                    score += 2;
+                    break;
+                case "Z":
+                    score += 3;
+                    break;
+            }
+            //Draw
+            if (opponentChoice == playerChoice)
+            {
+                score += 3;
+            }
+            else switch (opponentChoice)
+                // ReSharper disable once BadChildStatementIndent
+                {
+                    case "A" when playerChoice == "Y":
+                    case "B" when playerChoice == "Z":
+                    case "C" when playerChoice == "X":
+                        score += 6;
+                        break;
+                }
+
+            return score;
+        }
+
     }
 }
--- tests/Day2Tests/Day2Tests.cs
+++ tests/Day2Tests/Day2Tests.cs
@@ -6,10 +6,15 @@ namespace Day2Tests
 {
     public class Day2Tests
     {
+
+        private const string Input = "A Y";
+        //A for Rock, B for Paper, and C for Scissors.
+        //X for Rock, Y for Paper, and Z for Scissors
+
         [Test]
         public void Part1()
         {
-            RockPaperScissors.CalculatePart1("").Should().Be(0);
+            RockPaperScissors.CalculatePart1(Input).Should().Be(8);
         }
     }
 }
